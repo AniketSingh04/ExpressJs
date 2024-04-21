5b6386fc3db1831597388a5cd944ae38032e7ae3
AniketSingh04 <aniketrajsingh29@gmail.com>
2024-04-21 19:06:47 +0530

    JSON, PARAMS, QUERY

--- Express_JSON/data.js
+++ Express_JSON/data.js
@@ -1,21 +1,27 @@
 const products = [
     {
         id : 1,
-        name: "iphone",
+        name: "Apple",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDmg1d92NfyULgRdsK7BPLlJHMAjRHis8F2w&usqp=CAU",
         price: 86000
     },
     {
         id : 2,
-        name: "Samsung Galaxy Z-Flip",
+        name: "Samsung",
         image: "https://images.samsung.com/in/smartphones/galaxy-z-flip4/images/galaxy-z-flip4-share-image.jpg",
         price: 99999
     },
     {
         id : 3,
-        name: "Nokia Lumia",
+        name: "Aokia",
         image: "https://upload.wikimedia.org/wikipedia/commons/9/98/Nokia_Lumia_800_front.jpg",
         price: 34000
+    },
+    {
+        id : 4,
+        name: "HTC",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6aQj0xV7vLAtbx_6HIpDwWzBXP3mbgw3tCg&usqp=CAU",
+        price: 40000
     }
 ]
 
--- Express_JSON/index.js
+++ Express_JSON/index.js
@@ -6,6 +6,61 @@ app.get("/", (req, res)=>{
     res.send('<h1>Home</h1> <a href="/api/phone">Phones</a>'); //link created to visit the json file
 })
 
+
+/*
+//params(:)
+In Express.js, route parameters (or URL parameters) are
+placeholders in the URL pattern that match specific 
+parts of the URL. Route parameters(objects) are defined by 
+placing a colon (:) followed by the parameter name in the URL pattern.
+ */
+// app.get("/api/phone/:phoneID", (req, res)=>{
+//     const {phoneID} = req.params; // Access the parameter value using req.params
+//     console.log(req.params);
+//     const singleItem = phone.find((item)=> item.id ===  Number(phoneID));
+//     if(!singleItem){
+//         res.status(404).send("Item not found!!!")
+//     }
+//     res.json(singleItem);
+// })
+
+/*
+//query(?)
+In Express.js, query parameters are key-value
+pairs that appear in the URL after the ? character.
+They are used to provide additional data to the server in a request. 
+*/
+app.get("/api/v1/query", (req, res)=>{
+    let sortedPhone = [...phone];
+    const {search, limit} = req.query; //Query parameters are accessed through the req.query object in Express route handlers.
+    console.log(req.query); 
+
+    if(search){
+        sortedPhone = sortedPhone.filter((item)=> {
+            return item.name.startsWith(search); //items starting with a particular string will be filtered out in sortedPhone
+        });
+    }
+    if(limit && !isNaN(limit)){
+        sortedPhone = sortedPhone.slice(0, Number(limit)); //
+    }
+    /*the response will contain at most 'limit' no of items 
+    that match the search criteria.
+    If the limit parameter is not provided or is 
+    not a valid number, all matching items will be
+    returned without any limit. */
+
+    res.json(sortedPhone);
+    //res.send("Query in ExpressJs");
+});
+
+
+
+//accessing single product
+app.get("/api/phone/1", (req, res)=>{
+    const singleItems = phone.find((item)=> { return item.id === 1})
+    res.json(singleItems);
+})
+
 //redirects to the json file
 app.get("/api/phone", (req, res)=>{
     const newItems = phone.map((item)=>{ //mapping selected data
